84c9b4535a91b9221fb01a5f0f1e25cbcea91c0a
wing-lo <15623@coderacademy.edu.au>
2024-06-17 09:14:33 +1000

    Created init.py with JWT and database URI

--- src/app.py
+++ src/app.py
@@ -1,10 +1,24 @@
-from flask import Flask
-
-def create_app():
-    # A Flask app is created
-    app = Flask(__name__)
-    print(app.url_map)
-
-    return app
+from marshmallow.exceptions import ValidationError
+from init import app
 
 
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+    
+    
+@app.errorhandler(404)
+def not_found(err):
+    return {"error": "Not Found"}, 404
+
+
+@app.errorhandler(ValidationError)
+def invalid_request(err):
+    return {"error": vars(err)["messages"]}, 400
+
+
+@app.errorhandler(KeyError)
+def missing_key(err):
+    return {"error": f"Missing field: {str(err)}"}, 400
+
+print(app.url_map)
